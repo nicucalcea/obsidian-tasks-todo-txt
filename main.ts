d85589ea18c0bbe7c46d7bfc3dff11fff6caa867
nicucalcea <mail@nicu.md>
2023-04-10 01:15:03 +0100

    first version

--- main.ts
+++ main.ts
@@ -1,137 +1,124 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { Plugin } from 'obsidian';
 
-// Remember to rename these classes and interfaces!
+export default class TodoPlugin extends Plugin {
+  async onload() {
+    console.log('Loading Todo Plugin');
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    await this.updateTodoFile();
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
+    // Register an event listener for when a file is saved
+    this.registerEvent(
+      this.app.vault.on('modify', async (file) => {
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+        if (file.path === 'todo.txt') {
+          // Do not trigger if todo.txt is modified
+          return;
+        }
 
-	async onload() {
-		await this.loadSettings();
+        // Check if modified file has #todo tag
+        if ((await this.app.vault.read(file)).includes('#todo')) {
+          await this.updateTodoFile();
+        }
+      })
+    );
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    // Register an event listener for when a file is deleted
+    this.registerEvent(
+      this.app.vault.on('delete', async (file) => {
+        // Check if deleted file has #todo tag
+        if (file.path.includes('#todo')) {
+          await this.updateTodoFile();
+        }
+      })
+    );
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    // Register an event listener for when a new file is created
+    this.registerEvent(
+      this.app.vault.on('create', async (file) => {
+        // Check if created file has #todo tag
+        if ((await this.app.vault.read(file)).includes('#todo')) {
+          await this.updateTodoFile();
+        }
+      })
+    );
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+  }
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+  async updateTodoFile() {
+    // Get all the Markdown notes in the vault
+    const markdownFiles = this.getMarkdownFiles();
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+    // Filter out notes that do not have the #todo tag
+    const todoNotes = markdownFiles.filter(async (note) => {
+      const content = await this.app.vault.read(note);
+      return content.includes('#todo');
+    });
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+    const filteredTodoNotes = todoNotes.filter((note) => note !== undefined);
 
-	onunload() {
+    // Get the todo items from each note with #todo tag
+    const todoItems = filteredTodoNotes.map(async (note) => {
+      const items = (await this.app.vault.read(note))
+        ?.split('\n')
+        .filter((line) => (line.includes('- [ ]') || line.includes('- [x]')) && line.includes('#todo'))
+        .map((line) => {
+          // Replace Obsidian Tasks notations to todo.txt due date
+          line = line.replaceAll('/📅\s?/', 'due:');
 
-	}
+          // Replace Obsidian Tasks recurrence notation to todo.txt recurrence
+          line = line.replaceAll('🔁 every day', 'rec:1d')
+            .replaceAll('🔁 every week', 'rec:1w')
+            .replaceAll('🔁 every month', 'rec:1m');
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
+          // Replace Obsidian Tasks notations to todo.txt priority notation
+          const priorityRegex = /[\u{23EB}\u{1F53A}\u{1F53D}]/gu; // use 'g' flag to match all occurrences
+          const matches = [...line.matchAll(priorityRegex)];
+          let priority = '';
+          matches.forEach((match) => {
+            const priorityChar = match[0];
+            if (priorityChar === '\u{23EB}') {
+              priority = '(A)';
+            } else if (priorityChar === '\u{1F53A}') {
+              priority = '(B)';
+            } else if (priorityChar === '\u{1F53D}') {
+              priority = '(C)';
+            }
+            line = line.replace(priorityChar, '');
+          });
 
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
+          // Append the priority notation to the beginning of the line
+          if (priority) {
+            line = `${priority} ${line}`;
+          }
 
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
 
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
+          // Replace ticked off items with x
+          line = line.replace('- [x] ', 'x ');
 
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
+          // Remove the checkbox and any tags from the line
+          line = line.replace('- [ ]', '').replace('#todo', '');
 
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
+          return line.trim();
+        }) || [];
 
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
+      return items;
+    });
 
-	display(): void {
-		const {containerEl} = this;
+    // Wait for all promises to resolve before continuing
+    const allItems = await Promise.all(todoItems);
 
-		containerEl.empty();
+    // Update the todo.txt file with the latest todo items
+    await this.app.vault.adapter.write(
+      'todo.txt',
+      allItems.flat().join('\n') + '\n'
+    );
 
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
+    console.log('Updated todo.txt file');
+  }
 
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  getMarkdownFiles() {
+    const { vault } = this.app;
+    return vault.getMarkdownFiles();
+  }
 }
